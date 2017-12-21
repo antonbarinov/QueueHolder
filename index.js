cf3f7dbcfb3543f7ea5fe841067fb56963a32692
Anton <markelov.69@bk.ru>
2017-12-21 23:38:02 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+class QueueHolder {
+    constructor() {
+        this.__queue = [];
+        this.__busy = false;
+    }
+
+    async hold() {
+        if (this.__busy) {
+            let promise = new Promise((resolve) => {
+                this.__queue.push(resolve);
+            });
+
+            await promise;
+
+            return true;
+        }
+
+        this.__busy = true;
+
+        return true;
+    }
+
+    release() {
+        if (this.__queue.length) {
+            this.__queue.shift()(true);
+            return true;
+        } else  {
+            return false;
+        }
+    }
+
+    async sleep(seconds = 1) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(true);
+            }, seconds * 1000);
+        });
+    }
+}
+
+module.exports = QueueHolder;
